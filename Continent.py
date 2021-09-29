2b60ea3829617528ea4125dae102a80e794eb589
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:44:14 -0500

    Add files via upload

---
+++ Continent.py
@@ -0,0 +1,14 @@
+continents = [
+    'Asia',
+    'South America',
+    'North America',
+    'Africa',
+    'Europe',
+    'Antarctica',
+    'Australia',
+]
+for continent in continents:
+    :
+     A_continent = continent.startswith("A")
+        print('*', A_continent)
+
